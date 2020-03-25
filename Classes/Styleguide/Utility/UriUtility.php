3ae2ed41b6f152865a89a9ec16c6d873e4cf7c0d
tck <toby3@gmx.de>
2020-03-25 12:47:10 +0100

    adjust qr-code generation

--- Classes/Styleguide/Utility/UriUtility.php
+++ Classes/Styleguide/Utility/UriUtility.php
@@ -21,19 +21,18 @@ Class UriUtility {
 		return self::getQr( (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] ? "https://" : "http://") . $_SERVER['HTTP_HOST'].self::preview($ref, $sourceKey) );
 	}
 
-	public static function getQr( $url )
-	{
-		$qrCode = new QrCode();
-		return $qrCode
-		    ->setText( $url )
-		    ->setSize(100)
-		    ->setPadding(10)
-		    ->setErrorCorrection('high')
-		    ->setForegroundColor(array('r' => 0, 'g' => 0, 'b' => 0, 'a' => 0))
-		    ->setBackgroundColor(array('r' => 255, 'g' => 255, 'b' => 255, 'a' => 0))
-		    ->getDataUri()
-		;
 
-	}
+    public static function getQr( $url )
+    {
+        $qrCode = new QrCode();
+        $qrCode->setText($url);
+        $qrCode->setSize(150);
+        $qrCode->setMargin(10);
+        $qrCode->setErrorCorrectionLevel(ErrorCorrectionLevel::HIGH());
+        $qrCode->setForegroundColor(['r' => 0, 'g' => 0, 'b' => 0, 'a' => 0]);
+        $qrCode->setBackgroundColor(['r' => 255, 'g' => 255, 'b' => 255, 'a' => 0]);
+
+        return $qrCode->writeDataUri();
+    }
 
 }
